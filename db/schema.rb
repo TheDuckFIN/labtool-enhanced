3dc1969f9535e2e8a7ae16c467b01398eb2fbb0a
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-04-03 15:45:04 +0300

    added columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329135448) do
+ActiveRecord::Schema.define(version: 20160403124046) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20160329135448) do
     t.boolean  "admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "realname"
+    t.string   "email"
+    t.string   "irc_nick"
   end
 
 end
